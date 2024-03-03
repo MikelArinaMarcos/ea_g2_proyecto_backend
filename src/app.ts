f5dabac8bb9393754ac63cce82478726b20f1586
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2024-03-03 19:12:31 +0100

    /user route created

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { router } from "./routes/user";
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
-
-
+app.use(router);
 app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
