de7dc780fc0fbd440449b1905ce4add98d1d1602
borjafernandez35 <borjaf29@gmail.com>
2024-06-20 01:28:49 +0200

    sockets

--- lib/server.ts
+++ lib/server.ts
@@ -8,33 +8,29 @@ import { AuthJWT } from './middlewares/authJWT';
 
 const authJWT = new AuthJWT();
 const PORT = env.getPort();
-const agent = new HttpsProxyAgent('http://168.63.76.32:3128');
+const agent = new HttpsProxyAgent('https://front.spotfinder.duckdns.org');
 
 const server = app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
 });
 
 const io = new Server(server);
-//const wss = new WebSocket.Server({ server, clientTracking: true, agent });
+const wss = new WebSocket.Server({ server, clientTracking: true, agent });
 
 const connectedUser = new Set();
 
-  //wss.use((socket, next) => {
-io.use((socket, next) => {
+  wss.use((socket, next) => {
   console.log(socket.handshake, socket.handshake.auth.token);
   authJWT.verifyTokenSocket(socket, next);
 });
-//wss.on('connection', (socket) => {
-io.on('connection', (socket) => {
+wss.on('connection', (socket) => {
   console.log('Connected successfully', socket.id, socket.handshake.time);
   connectedUser.add(socket.id);
-  //wss.emit('connected-user', connectedUser.size);
-  io.emit('connected-user', connectedUser.size);
+  wss.emit('connected-user', connectedUser.size);
   socket.on('disconnect', () => {
     console.log('Disconnected successfully', socket.id);
     connectedUser.delete(socket.id);
-    io.emit('connected-user', connectedUser.size);
-    //wss.emit('connected-user', connectedUser.size);
+    wss.emit('connected-user', connectedUser.size);
   });
 
   socket.on('manual-disconnect', () => {
@@ -47,7 +43,7 @@ io.on('connection', (socket) => {
     data.userName = user.name;
 
     console.log(data);
-    //wss.broadcast.emit('message-receive', data);
-    socket.broadcast.emit('message-receive', data);
+    wss.broadcast.emit('message-receive', data);
+  
   });
 });
